813288c049a0c4f40ac526eb9ae245e5495f8bb0
karthik5033 <karthikkp5033@gmail.com>
2025-01-23 11:04:59 +0530

    added new files

---
+++ youtube classes/basics.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+//    char ch;
+//    cout<<"Enter the character : " ;
+//    cin>>ch;
+//    cout<<(int)ch;
+
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// float x = 5/2;
+// cout<<x;
+
+// }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+float x = 5/2;
+cout<<float(x);
+
+}
