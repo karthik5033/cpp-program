7ace1c36219b335d030a4bc33c25484205a95a0e
karthik5033 <karthikkp5033@gmail.com>
2026-01-13 11:29:41 +0530

    new codes

--- DSA/college/heapify.c
+++ DSA/college/heapify.c
@@ -1,1 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void heapify(int arr[],int n,int i){
+    int largest=i;
+    int l=2*i+1;
+    int r=2*i+2;
+    if(l<n && arr[l]>arr[largest]){
+        largest=l;
+    }
+    if(r<n && arr[r]>arr[largest]){
+        largest=r;
+    }
+    if(largest!=i){
+        int temp=arr[i];
+        arr[i]=arr[largest];
+        arr[largest]=temp;
+        heapify(arr,n,largest);
+    }
+}
