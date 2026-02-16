4d06720252dd7fa6b4f4c7800be0015176c561be
karthik5033 <karthikkp5033@gmail.com>
2026-02-16 23:18:07 +0530

    Added practice codes

---
+++ LeetCode/drill/removeX.cpp
@@ -0,0 +1,11 @@
+// Remove Target Value
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> removeTarget(vector<int>& nums, int target) {
+        
+    }
+};
