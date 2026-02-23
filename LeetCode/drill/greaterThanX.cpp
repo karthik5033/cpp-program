e5c29e60e0d1c336b010f100d8341d0697fdad57
karthik5033 <karthikkp5033@gmail.com>
2026-02-23 23:18:17 +0530

    a

--- LeetCode/drill/greaterThanX.cpp
+++ LeetCode/drill/greaterThanX.cpp
@@ -2,3 +2,23 @@
 
 // Concept: reference, loop, counter
 
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        vector<int> GreaterThanX(vector<int> &nums,auto x){
+            vector<int>result;
+            int count=0;
+            for(int y:nums){
+                if(y>x){
+                    result.push_back(x);
+                    count++;
+                }
+            }
+            return result;
+            return count;
+        }
+
+};
+
